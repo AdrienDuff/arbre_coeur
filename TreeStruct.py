bcfe46786129ad6a1c00ba0bf58f8e62ab646f80
benjamin schuler <benjamin schuler>
2017-04-06 16:17:56 +0200

    affichage arc de l'arbre

--- TreeStruct.py
+++ TreeStruct.py
@@ -1,7 +1,19 @@
 import numpy as np
 
 class TreeStruct(object):
-	"""docstring for GrapheStructure"""
-	def __init__(self, arg):
-		super (TreeStructure, self).__init__()
-		self.arg = arg
+	"""docstring for TreeStruct:
+	- nSommet nombre de sommets
+	- generation sa génération
+	- numero le numéro de l'arbre
+	- tree le tableau de structure"""
+
+	def __init__(self):
+		self.nSommet = 7
+		self.generation = 1
+		self.numero = 1
+		self.tree = [0,1,1,4,1,4,4,6]
+
+	def cout(self):
+		for i in range(2,self.nSommet+1):
+			print("arc :" + str(i) + str(self.tree[i]) )
+
--- hearth_trees.py
+++ hearth_trees.py
@@ -1,4 +1,9 @@
 import numpy as np
+from TreeStruct import*
+
+arbre =TreeStruct()
+print(arbre.tree[4])
+arbre.cout()
 
 
 
